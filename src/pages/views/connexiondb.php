8720186910d4296d6d3fdf5dd4d327f66094bbfa
Albert Paluku <20pm500@esisalama.org>
2024-11-18 15:43:42 +0100

    Tout est bon pour la prime et le congé(suite)

--- src/pages/views/connexiondb.php
+++ src/pages/views/connexiondb.php
@@ -1,11 +1,8 @@
 <?php
-function connect() {
-    try {
-        $bdd = new PDO("mysql:host=localhost;dbname=gestion_rh", "root", "");
-        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $bdd;
-    } catch (PDOException $e) {
-        die("Erreur de connexion : " . $e->getMessage());
-    }
+try{
+    $pdo = new PDO ("mysql:host=localhost;dbname=gestion_rh","root","");
+}catch(Exception $e){
+    die('Erreur de connexion :'.$e->getMessage());
 }
+
 ?>
